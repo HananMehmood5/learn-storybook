3fe3382c0f6ed97748103ea676eb094a09a8b8ff
HananMehmood5 <hanan.mehmood5@gmail.com>
2021-03-08 13:12:59 +0500

    feat: context for Tasks

---
+++ src/contexts/TasksContext.tsx
@@ -0,0 +1,99 @@
+import React, { createContext, useReducer } from "react";
+import { TASK_STATES, TaskType } from "../types";
+
+
+/****  Action Keys  ****/
+export enum TasksActionKeys {
+  onPinTask = "onPinTask",
+  onArchiveTask = "onArchiveTask",
+}
+
+/****  Action Types  ****/
+type TasksActionTypes = {
+  type: TasksActionKeys.onArchiveTask | TasksActionKeys.onPinTask;
+  id: string;
+}
+
+/****  State and Dispatch Props  ****/
+type TasksStateProps = {
+    loading: boolean;
+    tasks: TaskType[];
+}
+
+/****  Initial State  ****/
+const defaultTasks = [
+  { id: '1', title: 'Something', state: TASK_STATES.DEFAULT },
+  { id: '2', title: 'Something more', state: TASK_STATES.DEFAULT },
+  { id: '3', title: 'Something else', state: TASK_STATES.DEFAULT },
+  { id: '4', title: 'Something again', state: TASK_STATES.DEFAULT },
+];
+
+const initialState: TasksStateProps = { loading: false, tasks: defaultTasks };
+
+/****  Reducer  ****/
+const TasksReducer = (state: TasksStateProps = initialState, action: TasksActionTypes): TasksStateProps => {
+  switch (action.type) {
+    case TasksActionKeys.onArchiveTask:
+      return {
+        ...state,
+        tasks: state.tasks.map(
+            task => (task.id === action.id ? { ...task, state: TASK_STATES.TASK_ARCHIVED } : task)
+        ),
+      };
+    case TasksActionKeys.onPinTask:
+      return {
+        ...state,
+        tasks: state.tasks.map(
+            task => (task.id === action.id ? { ...task, state: TASK_STATES.TASK_PINNED } : task)
+        ),
+      };
+    
+    default:
+      return state;
+  }
+};
+
+/****  Context  ****/
+type TasksDispatchProps = {
+  onPinTask: (id: string) => void,
+  onArchiveTask: (id: string) => void,
+}
+
+type TasksContextType = {
+  state: TasksStateProps;
+  actions: TasksDispatchProps;
+}
+
+const TasksContext = createContext<TasksContextType>({
+    state: initialState,
+    actions: {
+        onPinTask: (id: string) => { },
+        onArchiveTask: (id: string) => { },
+    },
+});
+
+
+type TasksContextProviderProps = {
+  children: React.ReactNode
+}
+
+const TasksContextProvider = (props: TasksContextProviderProps) => {
+  const [state, dispatch] = useReducer(TasksReducer, initialState);
+  const contextValue = {
+    state,
+    actions: {
+      onPinTask: (id: string) => dispatch({ type: TasksActionKeys.onPinTask, id: id }),
+      onArchiveTask: (id: string) => dispatch({ type: TasksActionKeys.onArchiveTask, id: id }),
+    },
+  };
+
+  const { children } = props;
+  
+  return <TasksContext.Provider value={contextValue}>{children}</TasksContext.Provider>;
+}
+
+const TasksContextConsumer = TasksContext.Consumer;
+
+export {
+  TasksContext, TasksContextProvider, TasksContextConsumer
+};
