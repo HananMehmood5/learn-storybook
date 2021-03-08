4f0ea7e8fe8f3286a824cf50d03d69eb0636b4d9
HananMehmood5 <hanan.mehmood5@gmail.com>
2021-03-08 13:00:08 +0500

    feat: global decorator

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,14 @@
 
+import React from 'react';
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
 }
+
+export const decorators = [
+  (Story) => (
+    <div style={{ margin: '3em' }}>
+      <Story />
+    </div>
+  ),
+];
