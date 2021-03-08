16b258f2465787f6512ff4958eaec152e8d3dfd9
HananMehmood5 <hanan.mehmood5@gmail.com>
2021-03-08 16:13:33 +0500

    chore: context provider for componets

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import TaskList from "./containers/TasksList"
+import { TasksContextProvider } from "./contexts/TasksContext"
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <TaskList />
+        <TasksContextProvider>
+          <TaskList />
+        </TasksContextProvider>
       </header>
     </div>
   );
